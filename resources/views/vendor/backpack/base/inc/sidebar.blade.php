717c93ba282cdf4321479c95f900f21e00e6bf52
Lamenuit <thedarkmonk500@gmail.com>
2018-05-13 19:28:53 +0200

    ADD backpack permission manager

--- resources/views/vendor/backpack/base/inc/sidebar.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar.blade.php
@@ -18,6 +18,15 @@
           <!-- ======================================= -->
           {{-- <li class="header">Other menus</li> --}}
           <li><a href="{{ backpack_url('setting') }}"><i class="fa fa-cog"></i> <span>Settings</span></a></li>
+
+          <li class="treeview">
+            <a href="#"><i class="fa fa-group"></i> <span>Users, Roles, Permissions</span> <i class="fa fa-angle-left pull-right"></i></a>
+            <ul class="treeview-menu">
+              <li><a href="{{ url(config('backpack.base.route_prefix', 'admin') . '/user') }}"><i class="fa fa-user"></i> <span>Users</span></a></li>
+              <li><a href="{{ url(config('backpack.base.route_prefix', 'admin') . '/role') }}"><i class="fa fa-group"></i> <span>Roles</span></a></li>
+              <li><a href="{{ url(config('backpack.base.route_prefix', 'admin') . '/permission') }}"><i class="fa fa-key"></i> <span>Permissions</span></a></li>
+            </ul>
+          </li>
         </ul>
       </section>
       <!-- /.sidebar -->
