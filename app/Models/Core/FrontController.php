d570d25072fb8d1d06d8230f7c17c510becafc53
Lamenuit <thedarkmonk500@gmail.com>
2018-05-21 00:30:34 +0200

    UPDATE core frontcontroller => add init view logic

--- app/Models/Core/FrontController.php
+++ app/Models/Core/FrontController.php
@@ -12,6 +12,19 @@ class FrontController extends Controller
         return $this->_renderList($request);
     }
 
+    public function initView(Request $request, int $id, string $slug)
+    {
+        $this->_model = $this->modelName::find($id);
+        if ($this->_model === null) {
+            return redirect()->route($this->routePrefix.'-listing');
+        }
+
+        $this->_user = $request->user();
+
+        return $this->_renderView($request, $id, $slug);
+    }
+
     protected function _renderList(Request $request){}
+    protected function _renderView(Request $request, int $id, string $slug){}
 
 }
