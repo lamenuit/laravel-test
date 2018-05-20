5056b167cb7b3c146d3c4062933d757d47a70b14
Lamenuit <thedarkmonk500@gmail.com>
2018-05-20 23:01:36 +0200

    ADD novel zip processing

--- app/Http/Controllers/Front/MangaController.php
+++ app/Http/Controllers/Front/MangaController.php
@@ -6,10 +6,15 @@ use App\Models\Core\FrontController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 
+use App\Models\Manga;
+
 class MangaController extends FrontController
 {
     protected function _renderList(Request $request)
     {
+        $manga = Manga::find(1)->pages;
+        dd($manga);
+
         return View::make('front.manga.manga-list-core');
     }
 }
--- app/Models/Manga.php
+++ app/Models/Manga.php
@@ -3,11 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\UploadedFile;
 use Backpack\CRUD\CrudTrait;
+use Storage;
+
+use App\Models\Mpage;
 
 class Manga extends Model
 {
     use CrudTrait;
+
+    const EXTRACT_FOLDER = '/upload_buffer';
+    const ALLOWED_IMAGE_EXTENSIONS = array('jpg', 'png', 'jpeg');
 
     protected $table = 'mangas';
     protected $primaryKey = 'id';
@@ -18,10 +25,75 @@ class Manga extends Model
     /**************************************************************************
     *                               FUNCTIONS                                 *
     **************************************************************************/
+    public function processArchive(UploadedFile $archive): bool
+    {
+        // Clean extract folder
+        $files = glob(public_path('storage').self::EXTRACT_FOLDER.'/*');
+        foreach ($files as $file) {
+            if (is_file($file)) {
+                unlink($file);
+            }
+        }
+
+        // Extract new archive
+        $zip = new \ZipArchive();
+        if ($zip->open($archive) === true) {
+            if ($zip->extractTo(public_path('storage').self::EXTRACT_FOLDER)) {
+                $zip->close();
+            } else {
+                return false;
+            }
+        }
+
+        // Check and order files
+        $pages = array();
+        foreach (new \DirectoryIterator(public_path('storage').self::EXTRACT_FOLDER) as $fileInfo) {
+            if ($fileInfo->isDot()) {
+                var_dump('continue 1');
+                continue;
+            }
+
+            $fileName = $fileInfo->getFilename();
+            $extension = explode('.', $fileName);
+            if (!array_key_exists(1, $extension) || !in_array($extension[1], self::ALLOWED_IMAGE_EXTENSIONS)) {
+                if (is_file(public_path('storage').self::EXTRACT_FOLDER.'/'.$fileName)) {
+                    unlink(public_path('storage').self::EXTRACT_FOLDER.'/'.$fileName);
+                }
+                continue;
+            }
+            $pages[$fileName] = array(
+                'path' => public_path('storage').self::EXTRACT_FOLDER.'/'.$fileName,
+                'ext'  => $extension[1],
+                'name' => $fileName
+            );
+        }
+        usort($pages, function($a, $b){
+            return strnatcmp($a['path'],$b['path']);
+        });
+
+        // Create and move pages
+        foreach ($pages as $key => $pageData) {
+            $page = new Mpage();
+            $page->manga_id  = $this->id;
+            $page->extension = $pageData['ext'];
+            $page->number    = (int)($key + 1);
+            if ($page->save()) {
+                // var_dump($key);
+                // echo "<pre>";var_dump($page->getMPagePath(), $pageData['name']);echo "</pre>";
+                Storage::disk('local')->put($page->getMPagePath().$pageData['name'], file_get_contents($pageData['path']));
+            }
+        }
+
+        return true;
+    }
 
     /**************************************************************************
     *                               RELATIONS                                 *
     **************************************************************************/
+    public function pages()
+    {
+        return $this->hasMany('App\Models\Mpage');
+    }
 
     /**************************************************************************
     *                               SCOPES                                    *
