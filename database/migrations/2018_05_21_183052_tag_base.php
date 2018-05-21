42235e4e5928f0adf25f5d7d657f79aabc358be2
Lamenuit <thedarkmonk500@gmail.com>
2018-05-21 20:36:15 +0200

    ADD tag base

---
+++ database/migrations/2018_05_21_183052_tag_base.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TagBase extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function($table){
+            $table->increments('id');
+            $table->string('name');
+
+            $table->index('name');
+        });
+
+        Schema::create('manga_tag', function($table){
+            $table->integer('manga_id')->unsigned();
+            $table->integer('tag_id')->unsigned();
+
+            $table->foreign('manga_id')->references('id')->on('mangas')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::DropIfExists('manga_tag');
+        Schema::DropIfExists('tags');
+    }
+}
