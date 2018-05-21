9079bf4052879a8c627d551f89d01e5a9c9f7e4f
Lamenuit <thedarkmonk500@gmail.com>
2018-05-21 21:37:40 +0200

    ADD layout view composer + load css and js libs

---
+++ app/Http/ViewComposers/LayoutComposer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\ViewComposers;
+
+use Illuminate\View\View;
+
+class LayoutComposer
+{
+    const JS_LIBS = [
+        'js/libs/jquery.min.js'
+    ];
+    const CSS_LIBS = [
+        'css/libs/bootstrap.min.css'
+    ];
+
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * Bind data to the view.
+     *
+     * @param  View  $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        $view->with([
+            'js_libs'  => self::JS_LIBS,
+            'css_libs' => self::CSS_LIBS
+        ]);
+    }
+}
